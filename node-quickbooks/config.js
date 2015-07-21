6385e2da02ffb941693fe9644c6ac7731ab96865
eaglesumit <sumit@eagleindustrialgroup.com>
2015-07-21 14:14:06 -0700

    Adding Files for testing

---
+++ node-quickbooks/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  consumerKey:     'qyprdz0DNrdXGJ6zLDUdh76pbhhqve',
+  consumerSecret:  'gX3Ms0YA9Or4x0XAm9wdax43EsAqY8gMKwZBL9WX',
+  token:           'fefdd201bf931b49ccbab4ebc9702ac40d74',
+  tokenSecret:     '',
+  realmId:         '182276574',
+  useSandbox:      true,
+  debug:           false,
+  //
+  // Set useSandbox to false when moving to production. For info, see the following url:
+  // https://developer.intuit.com/v2/blog/2014/10/24/intuit-developer-now-offers-quickbooks-sandboxes
+  
+  testEmail:       'sumit@eagleindustrialgroup.com'  // Use this email address for testing send*Pdf functions
+}
